1453f356472519353ad4320392056e9e35f3fb67
nehCnitsuJ <s10222137@connect.np.edu.sg>
2022-11-15 16:27:25 +0800

    halfway complete part 2

--- contactjs.js
+++ contactjs.js
@@ -1,4 +1,5 @@
-const firebaseConfig = {
+
+    const firebaseConfig = {
     apiKey: "AIzaSyAwG7RePlLNiGGfs61BttyXserKakBlq74",
     authDomain: "pfd-asg-1.firebaseapp.com",
     databaseURL: "https://pfd-asg-1-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -7,47 +8,47 @@ const firebaseConfig = {
     messagingSenderId: "683817910117",
     appId: "1:683817910117:web:6bc464138c242c9853eb8c",
     measurementId: "G-PCCLL01552"
-};
+    };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-// reference your database
-var contactFormDB = firebase.database().ref("contactForm");
+    // reference your database
+    var contactFormDB = firebase.database().ref("contactForm");
 
-document.getElementById("contactForm").addEventListener("submit", submitForm);
+    document.getElementById("contactForm").addEventListener("submit", submitForm);
 
-function submitForm(e) {
-  e.preventDefault();
+    function submitForm(e) {
+    e.preventDefault();
 
-  var name = getElementVal("name");
-  var emailid = getElementVal("emailid");
-  var msgContent = getElementVal("msgContent");
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var msgContent = getElementVal("msgContent");
 
-  saveMessages(name, emailid, msgContent);
+    saveMessages(name, emailid, msgContent);
 
-  //   enable alert
-  document.querySelector(".alert").style.display = "block";
+    //   enable alert
+    document.querySelector(".alert").style.display = "block";
 
-  //   remove the alert
-  setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-  }, 3000);
+    //   remove the alert
+    setTimeout(() => {
+        document.querySelector(".alert").style.display = "none";
+    }, 3000);
 
-  //   reset the form
-  document.getElementById("contactForm").reset();
-}
+    //   reset the form
+    document.getElementById("contactForm").reset();
+    }
 
-const saveMessages = (name, emailid, msgContent) => {
-  var newContactForm = contactFormDB.push();
+    const saveMessages = (name, emailid, msgContent) => {
+    var newContactForm = contactFormDB.push();
 
-  newContactForm.set({
-    name: name,
-    emailid: emailid,
-    msgContent: msgContent,
-  });
-};
+    newContactForm.set({
+        name: name,
+        emailid: emailid,
+        msgContent: msgContent,
+    });
+    };
 
-const getElementVal = (id) => {
-  return document.getElementById(id).value;
-};
+    const getElementVal = (id) => {
+    return document.getElementById(id).value;
+    };
--- upload.js
+++ upload.js
@@ -1,14 +1,100 @@
-function RefreshCanceller(event) {
-    event.preventDefault();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwG7RePlLNiGGfs61BttyXserKakBlq74",
+  authDomain: "pfd-asg-1.firebaseapp.com",
+  databaseURL: "https://pfd-asg-1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pfd-asg-1",
+  storageBucket: "pfd-asg-1.appspot.com",
+  messagingSenderId: "683817910117",
+  appId: "1:683817910117:web:6bc464138c242c9853eb8c",
+  measurementId: "G-PCCLL01552"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL }
+from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js"
+
+// Variables and references
+
+var files = [];
+var reader = new FileReader();
+
+var namebox = document.getElementById('namebox');
+var extlab = document.getElementById('extlab');
+var myimg = document.getElementById('myimg');
+var proglab = document.getElementById('upprogress');
+var SelBtn = document.getElementById('selbtn');
+var UpBtn = document.getElementById('upbtn');
+var DownBtn = document.getElementById('downbtn');
+
+var input = document.createElement('input');
+input.type = 'file';
+
+input.onchange = e =>{
+  files = e.target.files;
+
+  var extension = GetExtName(files[0]);
+  var name = GetFileName(files[0]);
+
+  namebox.value= name;
+  extlab.innerHTML = extension;
+
+  reader.readAsDataURL(files[0]);
+}
+
+reader.onload = function(){
+  myimg.src = reader.result;
+}
+
+// Selecting the image
+
+SelBtn.onclick = function(){
+  input.click();
+}
+
+function GetExtName(file) {
+  var temp = file.name.split('.');
+  var ext = temp.slice((temp.length-1),(temp.length));
+  return '.' + ext[0];
+}
+
+function GetFileName(file) {
+  var temp = file.name.split('.');
+  var fname = temp.slice(0,-1).join('.');
+  return fname;
+}
+
+// Upload process
+async function UploadProcess() {
+  var ImgToUpload = files[0];
+
+  var ImgName = namebox.value + extlab.innerHTML;
+
+  const metaData = {
+    contentType: ImgToUpload.type
   }
+
+  const storage = getStorage();
   
-  function UploadHandler(event) {
-    document.querySelector('#img-display').src = URL.createObjectURL(
-      event.target.files[0]
-    );
+  const stroageRef = sRef(storage, "Images/"+ImgName);
+
+  const UploadTask = uploadBytesResumable(stroageRef, ImgToUpload, metaData);
+
+  UploadTask.on('state-changed', (snapshot)=>{
+    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    proglab.innerHTML = "Upload "+ progress + "%";
+  },
+  (error) =>{
+    alert("Error: Image has failed to upload!");
+  },
+  ()=>{
+    getDownloadURL(UploadTask.snapshot.ref).then((downloadURL)=>{
+      console.log(downloadURL);
+    });
   }
-  
-  function UploadFormSubmitHandler(event) {
-    event.preventDefault();
-    alert('Form submitted!');
-  }
+  );
+}
+UpBtn.onclick = UploadProcess;
