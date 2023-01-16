a27761624af841d8aac245495e45f62fde2b472e
nehCnitsuJ <s10222137@connect.np.edu.sg>
2023-01-16 13:44:39 +0800

    implemented chatbot functionality

---
+++ response.js
@@ -0,0 +1,12 @@
+const responseObj = {
+  Hello:
+    "Hi I am NgeeBot! I will be answering some commonly asked questions. Type the alphabet below and I will answer the respective questions!",
+  Hey: "Hi I am NgeeBot! I will be answering some commonly asked questions. Type the alphabet below and I will answer the respective questions!",
+  Hi: "Hi I am NgeeBot! I will be answering some commonly asked questions. Type the alphabet below and I will answer the respective questions!",
+  A: "No you do not need to meet up with us. It is similar to the Carousell app, however, you do need to drop your electronics into the box at the designated location.",
+  B: "The box for donations will be placed inside the Capstone Project room at Blk 31 Level 5. An E-vending machine is in talks to be implemented, stay tune for that!",
+  C: "There will be a QR code for you to scan to attain CCA points for participation. Donors will be automatically entered into a giveaway where you stand a chance to win a gift card of your choice at the end of the semester.",
+  D: "Usable electronic that has not turned obsolete will be up for sale at a very low price (ranging from $3-$8, depending on the type of electronic taken). This sale will be held on Week 6 and Week 15 of every semester, do check your email when the dates come closer!",
+  E: "Yes you can buy from us before the sale begins, simply contact us at the Contact Us! section of the website",
+  Thanks: "You're Welcome!",
+};
