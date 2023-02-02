784f6792f841cba974c400c87d4308312dd3b712
Dyoneus <s10227735@connect.np.edu.sg>
2023-02-03 00:29:08 +0800

    Update: Form won't require password box to be filled in order for display name to be updated.

--- editpfp.js
+++ editpfp.js
@@ -33,37 +33,6 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
   });
 
-// // -----PROFILE PICTURE------
-// firebase.auth().onAuthStateChanged(function(user) {
-//     if (user) {
-//         // User is signed in.
-//         var storageRef = firebase.storage().ref();
-//         var profilePictureRef = storageRef.child(`users/${user.uid}/profile_picture`);
-//         // Code to update profile picture goes here
-//         // STORING PROFILE PICTURE IMAGE TO FIREBASE DB
-//         document.getElementById("profilePictureInput").addEventListener("change", function(event) {
-//         var file = event.target.files[0];
-
-//         profilePictureRef.put(file).then(function(snapshot) {
-//             console.log("Profile picture uploaded successfully");
-//         }).catch(function(error) {
-//             console.error("Error uploading profile picture:", error);
-//         });
-
-//         profilePictureRef.getDownloadURL().then(function(url) {
-//             firebase.database().ref("/users/" + user.uid).update({
-//                 profilePictureUrl: url
-//             });
-//         }).catch(function(error) {
-//             console.error("Error getting download URL for profile picture:", error);
-//         });
-//   });
-//     } else {
-//       // No user is signed in.
-//     }
-//   });
-
-
 // Reference to the Firebase authentication instance
 var auth = firebase.auth();
 // Submit button event listener
@@ -73,42 +42,78 @@ submitButton.addEventListener("click", function() {
   var newPassword = newPasswordInput.value;
   var displayName = displayNameInput.value
 
-//   // Validate the new password
-//   if (newPassword.length < 6) {
-//     // Show error message if the new password is too short
-//     console.error("Error: New password must be at least 6 characters long");
-//     return;
-//   }
-
   // Get the current user
   var user = auth.currentUser;
   // Sign in the user with their current password
-  auth.signInWithEmailAndPassword(user.email, currentPassword)
-    .then(function(result) {
-      // Update the user's display name
-      result.user.updateProfile({
+  if (newPassword) {
+    // Sign in the user with their current password
+    auth.signInWithEmailAndPassword(user.email, currentPassword)
+      .then(function(result) {
+        if (displayName) {
+          result.user.updateProfile({
+            displayName: displayName
+          })
+          .then(function() {
+            console.log("Display name updated successfully");
+            Swal.fire({
+              icon: "success",
+              text: "Display name updated successfully!",
+            });
+            document.getElementById("editPfp").reset(); // Remove all credentials in the form after successful register
+          })
+          .catch(function(error) {
+            console.error("Error updating display name:", error);
+            Swal.fire({
+              icon: "error",
+              text: "Error updating display name! Please check your entries again.",
+            });
+          });
+        }
+  
+        result.user.updatePassword(newPassword)
+          .then(function() {
+            console.log("Password updated successfully");
+            Swal.fire({
+              icon: "success",
+              text: "Password updated successfully!",
+            });
+            document.getElementById("editPfp").reset(); // Remove all credentials in the form after successful register
+          })
+          .catch(function(error) {
+            console.error("Error updating password:", error);
+            Swal.fire({
+              icon: "error",
+              text: "Error updating password! Please check your entries again.",
+            });
+          });
+      })
+      .catch(function(error) {
+        console.error("Error signing in:", error);
+        Swal.fire({
+          icon: "error",
+          text: "Error updating password! Please check your entries again.",
+        });
+      });
+  } else {
+    if (displayName) {
+      user.updateProfile({
         displayName: displayName
       })
-        .then(function() {
-          console.log("Display name updated successfully");
-
-          // Check if the new password value is not empty
-          if (newPassword) {
-            // Update the user's password
-            result.user.updatePassword(newPassword)
-              .then(function() {
-                console.log("Password updated successfully");
-              })
-              .catch(function(error) {
-                console.error("Error updating password:", error);
-              });
-          }
-        })
-        .catch(function(error) {
-          console.error("Error updating display name:", error);
+      .then(function() {
+        console.log("Display name updated successfully");
+        Swal.fire({
+          icon: "success",
+          text: "Display name updated successfully!",
         });
-    })
-    .catch(function(error) {
-      console.error("Error signing in:", error);
-    });
+        document.getElementById("editPfp").reset(); // Remove all credentials in the form after successful register
+      })
+      .catch(function(error) {
+        console.error("Error updating display name:", error);
+        Swal.fire({
+          icon: "error",
+          text: "Error updating display name! Please check your entries again.",
+        });
+      });
+    }
+  }
 });
